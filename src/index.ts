c710286bbcc6e9c00a5c7db2c30d4c15f8630b53
Alan Martins <alanhorcrux@gmail.com>
2021-05-12 14:51:10 -0300

    Configurado TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-const express = require('express');
+import express from "express";
 
 const app = express();
 
-app.get("/",(request, response)=>{
+app.get("/",(request, response)=> {
     return response.json({message: "Ok"})
 });
 
