c0b3faf71a0f9192ceea52d56834b38dac24b891
Alan Martins <alanhorcrux@gmail.com>
2021-05-12 15:52:32 -0300

    rm dev

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Ok Dev" });
+  return response.json({ message: "Ok" });
 });
 
 app.listen("3333");
