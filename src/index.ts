1841af914e1bfdb54ce51cfe78113052cdfd45a0
Alan Martins <alanhorcrux@gmail.com>
2021-05-12 15:05:46 -0300

    Criado branch de Dev

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.json({ message: "Ok" });
+  return response.json({ message: "Ok Dev" });
 });
 
 app.listen("3333");
