5f723e572d212b58143bdc46a6a9bad9cd391fb1
Alan Martins <alanhorcrux@gmail.com>
2021-07-27 13:31:56 -0300

    Ajuste seed ShopCart

--- server/src/database/seed/admin.ts
+++ server/src/database/seed/admin.ts
@@ -7,11 +7,17 @@ async function create() {
   const connection = await createConnection("localhost");
 
   const id = uuidV4();
+  const idShop = uuidV4();
   const password = await hash("admin", 8);
 
   await connection.query(`
     INSERT INTO "User"("Id","Name","Email","Password","DocumentType","Document","IsActive","IsAdmin","Phone","Mobile","Created_at")
     VALUES ('${id}','Admin','admin@imaginebox.com.br','${password}','CNPJ','96710738000111',true,true,'1156628116','11966179541', 'now()')
+  `);
+
+  await connection.query(`
+    INSERT INTO "ShopCart"("Id","UserId")
+    VALUES ('${idShop}','${id}')
   `);
 
   await connection.close();
