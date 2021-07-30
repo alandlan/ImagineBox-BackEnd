9344685d6ab4b32deeb4a1d71c6869ab393cfb64
Alan Martins <alanhorcrux@gmail.com>
2021-07-30 11:22:31 -0300

    Add axios e cors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
-
+import { useEffect } from 'react';
+import {api} from './services/api'
 
 export function App() {
+
+  useEffect(()=> {
+    api.get("/catalogue/90d054b0-c727-4d75-aa03-d39c4dcd56ea/products").then(data => console.log(data))
+  },[])
+
   return (
     <div className="App">
       <h1>Hello Word</h1>
