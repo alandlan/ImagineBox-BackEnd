35ad960c4aca1894d991ee020fc1eccbecc0253a
Alan Martins <alanhorcrux@gmail.com>
2021-07-30 15:46:53 -0300

    add styles

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+        <title>Imagine Box - Home</title>
+    </Head>
+    <h1>
+      
+      Imagine <span>Box</span>
+    </h1>
+    </>
+  )
+} 
