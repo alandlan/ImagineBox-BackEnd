3451f216c0bec0e8625fe7f0691776b04a7a28ca
Alan Martins <alanhorcrux@gmail.com>
2021-08-02 19:14:01 -0300

    Tela Login

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,27 +1,7 @@
-import { Flex,Button, Stack, FormLabel,FormControl } from '@chakra-ui/react'
-import { Input } from '../components/Form/Input'
+import { Header} from '../components/Header'
 
-export default function Home() {
-  return (
-    <Flex w="100wh" h="100vh" align="center" justify="center">
-      <Flex 
-        as="form" 
-        width="100%" 
-        maxWidth={360}
-        bg="orange.300"
-        p="8"
-        borderRadius={8} 
-        flexDir="column"
-        >
-          <Stack spacing="4">
-
-            <Input name="email" type="email" label="E-mail" />
-            <Input name="password" type="password" label="Password" />
-            
-          </Stack>
-
-          <Button type="submit" mt="6" size="lg" colorScheme="orange">Entrar</Button>
-      </Flex>
-    </Flex>
+export default function Home(){
+  return(
+    <Header />
   )
-} 
+}
