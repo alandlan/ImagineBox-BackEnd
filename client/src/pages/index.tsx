793c36af5dc2bfa4404fa85c5b58cb52b987293e
Alan Martins <alanhorcrux@gmail.com>
2021-08-02 18:38:21 -0300

    Tela Login

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Flex,Input,Button, Stack, FormLabel,FormControl } from '@chakra-ui/react'
+import { Flex,Button, Stack, FormLabel,FormControl } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
 
 export default function Home() {
   return (
@@ -9,38 +10,15 @@ export default function Home() {
         maxWidth={360}
         bg="orange.300"
         p="8"
-        borderRadius={8}
+        borderRadius={8} 
         flexDir="column"
         >
           <Stack spacing="4">
-            <FormControl>
-              <FormLabel htmlFor="email">E-mail</FormLabel>
-              <Input 
-                type="email" 
-                name="email"
-                focusBorderColor="orange.900" 
-                bgColor="orange.200"
-                variant="filled"
-                _hover={{
-                  bgColor:"orange.200"
-                }}
-                size="lg"
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="password">Senha</FormLabel>
-              <Input 
-                type="password" 
-                name="password" 
-                focusBorderColor="orange.900" 
-                bgColor="orange.200"
-                variant="filled"
-                _hover={{
-                  bgColor:"orange.200"
-                }}
-                />
-            </FormControl>
-            </Stack>
+
+            <Input name="email" type="email" label="E-mail" />
+            <Input name="password" type="password" label="Password" />
+            
+          </Stack>
 
           <Button type="submit" mt="6" size="lg" colorScheme="orange">Entrar</Button>
       </Flex>
