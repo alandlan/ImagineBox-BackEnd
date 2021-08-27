3454614653064a4695f2eccfc843742c5883890c
Alan Martins <alanhorcrux@gmail.com>
2021-08-27 09:24:32 -0300

    Migration Order

---
+++ server/src/database/migrations/1630067032179-CreateOrder.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateOrder1630067032179 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
